fad8a1bbf310cc67a3fac164afca6ee3b292cc99
drifitngpear <ryanyohochan@gmail.com>
2026-03-16 09:21:42 -0400

    small fix to CNAME

--- .eleventy.js
+++ .eleventy.js
@@ -59,7 +59,7 @@ module.exports = async function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/fonts");
-  eleventyConfig.addPassthroughCopy("CNAME");
+  eleventyConfig.addPassthroughCopy("./src/CNAME");
 
   return {
     dir: {
