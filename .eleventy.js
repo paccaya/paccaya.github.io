27205691ca948940f1d44929fb613c0dd8a92a51
drifitngpear <ryanyohochan@gmail.com>
2026-03-16 09:20:23 -0400

    CNAME

--- .eleventy.js
+++ .eleventy.js
@@ -59,6 +59,7 @@ module.exports = async function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/fonts");
+  eleventyConfig.addPassthroughCopy("CNAME");
 
   return {
     dir: {
