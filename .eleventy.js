6a01ef1c46bf8bc5c75bda2ce3b09b0d9bb01a94
drifitngpear <ryanyohochan@gmail.com>
2026-04-22 17:41:58 -0400

    Create favicon and update to home.njk

--- .eleventy.js
+++ .eleventy.js
@@ -58,6 +58,7 @@ module.exports = async function(eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
   eleventyConfig.addPassthroughCopy("./src/fonts");
   eleventyConfig.addPassthroughCopy("./src/CNAME");
 
